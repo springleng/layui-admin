311cb89f7d5238d2664b6b1d03d96c16e460a04b
silen <silence.lc@gmail.com>
2017-05-10 17:03:15 +0800

    添加表格

--- res/js/modules/index.js
+++ res/js/modules/index.js
@@ -1,12 +1,23 @@
 layui.define(['layer', 'form', 'element'], function (exports) {
-    var layer = layui.layer,
+    var $ = layui.jquery,
+        layer = layui.layer,
         form = layui.form(),
         element = layui.element();
     layer.msg('Hello World');
 
+    // Form submit
     form.on('submit(formDemo)', function (data) {
         layer.msg(JSON.stringify(data.field));
         return false;
+    });
+
+    // CheckBox check all
+    form.on('checkbox(allChoose)', function (data) {
+        var child = $(data.elem).parents('table').find('tbody input[type="checkbox"]');
+            child.each(function (index, item) {
+            item.checked = data.elem.checked;
+        });
+        form.render('checkbox');
     });
 
     exports('index', {});
