af3e793821bd13416c53b1ebf1a7373feab9feec
silen <silence.lc@gmail.com>
2017-05-10 16:55:34 +0800

    添加表单

--- res/js/modules/index.js
+++ res/js/modules/index.js
@@ -1,6 +1,13 @@
-layui.define(['layer', 'form'], function (exports) {
+layui.define(['layer', 'form', 'element'], function (exports) {
     var layer = layui.layer,
-        form = layui.form();
+        form = layui.form(),
+        element = layui.element();
     layer.msg('Hello World');
+
+    form.on('submit(formDemo)', function (data) {
+        layer.msg(JSON.stringify(data.field));
+        return false;
+    });
+
     exports('index', {});
 });
